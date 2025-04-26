0f58b0c496199506c2b29bdbbcb80c8d28412ea9
amyavi <144570677+amyavi@users.noreply.github.com>
2025-04-26 12:22:26 -0300

    refactor: cleanup docker setup

--- docker/minecraft/build.sh
+++ docker/minecraft/build.sh
@@ -3,14 +3,15 @@ set -xe
 readonly PACK_FILE=/pack/pack.toml
 
 java -cp /opt/packwiz-installer.jar link.infra.packwiz.installer.DevMainKt \
-    --no-gui --side server \
+    --no-gui \
+    --side server \
     "file://$PACK_FILE"
 
 _VERSIONS="$(awk -F '[ ="]+' '/[versions]/ && ($1 == "fabric" || $1 == "minecraft") {print $1"="$2}' "$PACK_FILE")"
 _MINECRAFT_VERSION="$(echo "$_VERSIONS" | awk -F '=' '$1 == "minecraft" {print $2}')"
-[ -z "$_MINECRAFT_VERSION" ] && (printf '_MINECRAFT_VERSION was empty\n'; exit 1)
+[ -z "$_MINECRAFT_VERSION" ] && (printf 'Failed to determine MC version\n'; exit 1)
 _FABRIC_VERSION="$(echo "$_VERSIONS" | awk -F '=' '$1 == "fabric" {print $2}')"
-[ -z "$_FABRIC_VERSION" ] && (printf '_FABRIC_VERSION was empty\n'; exit 1)
+[ -z "$_FABRIC_VERSION" ] && (printf 'Failed to determine loader version\n'; exit 1)
 
 _REMAPPED_FOLDER_NAME="minecraft-$_MINECRAFT_VERSION-$_FABRIC_VERSION"
 if [ ! -d ".fabric/remappedJars/$_REMAPPED_FOLDER_NAME" ]; then
